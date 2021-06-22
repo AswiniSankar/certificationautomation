4012d0f839f20d9592a31f01d2ac79cd23052b75
Aswini Sankar <53120389+AswiniSankar@users.noreply.github.com>
2021-06-22 16:46:10 +0530

    Update main.py

--- certificateAutomation/main.py
+++ certificateAutomation/main.py
@@ -4,14 +4,14 @@ import pandas as pd
 import os
 
 df = pd.read_csv('list3.csv')
-font_path = '//../fonts/DroidSerif-Bold.ttf'
-assert os.path.isfile(font_path)
-font = ImageFont.truetype(font_path, 80)
-for index, j in df.iterrows():
+font_path = '//../fonts/DroidSerif-Bold.ttf'  #locate .ttf - this command used to locate the installed font in your system from that various select one font path
+assert os.path.isfile(font_path) #check for the font path
+font = ImageFont.truetype(font_path, 80) #here font size is 80
+for index, j in df.iterrows(): #index is represents each row and j represents cell value 
     # print(index)
-    img = Image.open('certificate.jpg')
-    draw = ImageDraw.Draw(img)
-    draw.text(xy=(900, 765), text='{}'.format(j['name']), fill=(0, 0, 255), font=font)
-    draw.text(xy=(300, 765), text='{}'.format(j['cd']), fill=(0, 0, 255), font=font)
+    img = Image.open('certificate.jpg') #certificate template  is opened 
+    draw = ImageDraw.Draw(img) #ready to write in the template
+    draw.text(xy=(900, 765), text='{}'.format(j['name']), fill=(0, 0, 255), font=font) #fill is for font color and font is for font type
+    draw.text(xy=(300, 765), text='{}'.format(j['cd']), fill=(0, 0, 255), font=font)# here the coordinates are given to write the text
     draw.text(xy=(300, 1000), text='{}'.format(j['ceo']), fill=(0, 0, 255), font=font)
-    img.save('pictures/{}.jpg'.format(j['name']))
+    img.save('pictures/{}.jpg'.format(j['name'])) #writen certificates are saved in the picture folder with the name of student itself
